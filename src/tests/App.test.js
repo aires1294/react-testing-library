cb9000873b78bd5df8c4dc6096fa32b178db09ee
aires1294 <pedrotja.engenharia@gmail.com>
2022-09-20 15:23:03 -0300

    item 1 feitoooo

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste se o topo da aplicação contém um conjunto fixo de links: Home ', () => {
+  renderWithRouter(<App />);
+  const linkElement = screen.getByRole('link', { name: /Home/i });
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Teste se o topo da aplicação contém um conjunto fixo de links: About ', () => {
+  renderWithRouter(<App />);
+  const linkElement = screen.getByRole('link', { name: /About/i });
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Teste se o topo da aplicação contém: Favorite Pokémons ', () => {
+  renderWithRouter(<App />);
+  const linkElement = screen.getByRole('link', { name: /Favorite Pokémons/i });
+  expect(linkElement).toBeInTheDocument();
+});
