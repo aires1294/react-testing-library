5c3e6234a7d3ac8e96410737aa81347b86149662
aires1294 <pedrotja.engenharia@gmail.com>
2022-09-21 14:48:50 -0300

    item 3 feitooo

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+// import { render } from '@testing-library/react';
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import App from '../App';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+// import userEvent from '@testing-library/user-event';
+
+test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+  renderWithRouter(<FavoritePokemons />);
+  const text = screen.getByText(/No favorite pokemon found/i);
+  expect(text).toBeInTheDocument();
+});
